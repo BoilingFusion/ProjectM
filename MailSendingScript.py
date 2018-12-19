57ff6137bc30e6af0163173ef328ef2025516f58
julian adamse <julian@julians-MacBook-Air.local>
2018-12-19 17:02:36 +0100

    logging in
    het inloggen werkt nu

--- MailSendingScript.py
+++ MailSendingScript.py
@@ -1,10 +1,30 @@
 import email
-
-
+import smtplib
 
 
 target = input("target:")
 name = input("name:")
 code = input("code:")
 
+try:
+    print("connection to server")
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    print("logging into server")
+    s.login(name,code)
+    print("logged in and ready to go")
 
+except:
+    print("mission failed, better luck next time")
+    exit()
+
+
+
+
+
+
+
+print("closing connection")
+s.close()
+print("done")
