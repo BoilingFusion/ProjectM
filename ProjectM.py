44256579714d3eb430dd7eb7b891c02e39272f85
BoilingFusion <45972321+BoilingFusion@users.noreply.github.com>
2018-12-20 16:46:11 +0100

    Update ProjectM.py

--- ProjectM.py
+++ ProjectM.py
@@ -1,1 +1,12 @@
+from MailSendingScript import *
+from reddit_quote_extractor import *
+import time
 
+try:
+    time.sleep(5)
+    SendMail(get_reddit())
+    print("mission sucsesfull")
+    
+except:
+    CloseConnection()
+    print("mission failed, better luck next time")
